a8ecd334fb09a286afaff90da7f00efdebe07693
gregLibert <greg_libert@yahoo.fr>
2023-01-15 15:45:54 +0100

    adds stdout and stderr messages

--- thyra-plugin-hello-world.go
+++ thyra-plugin-hello-world.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
+	ticker := time.NewTicker(5 * time.Second)
+
+	intSig := make(chan os.Signal, 1)
+	signal.Notify(intSig, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Fprintf(os.Stderr, "Plugin is initializing.\n")
+
 	for {
+		select {
+		case <-ticker.C:
+			fmt.Fprintf(os.Stdout, "Tic!\n")
+		case <-intSig:
+			fmt.Fprintf(os.Stderr, "Plugin is shutting down.\nBye!\n")
+			return
+		}
 	}
 }
-
