8eb63edc8f9c998c27abedf753ad79552d7abc84
gregLibert <greg_libert@yahoo.fr>
2023-01-20 16:40:15 +0100

    adds register

--- thyra-plugin-hello-world.go
+++ thyra-plugin-hello-world.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,6 +19,56 @@ import (
 	"github.com/massalabs/thyra-plugin-hello-world/api/server/restapi/operations"
 	"github.com/massalabs/thyra-plugin-hello-world/web"
 )
+
+const ThyraRegisterEndpoint = "https://my.massa/plugin-manager/register"
+
+type Register struct {
+	ID          int64
+	Name        string
+	Description string
+	Logo        string
+	Authority   string
+	APISpec     string
+}
+
+func register(pluginID int64, authority net.Addr, spec string) {
+	logo, err := web.Content("logo_massa.webp")
+	if err != nil {
+		panic(err)
+	}
+
+	reg := Register{
+		ID:          pluginID,
+		Name:        "hello world",
+		Description: "A simple hello world plugin.",
+		Authority:   "http://" + authority.String(),
+		APISpec:     spec,
+		Logo:        base64.StdEncoding.EncodeToString(logo),
+	}
+
+	body, err := json.Marshal(reg)
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest("POST", ThyraRegisterEndpoint, bytes.NewBuffer(body))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+
+	if response.StatusCode == http.StatusNoContent {
+		fmt.Fprintf(os.Stdout, "Plugin successfuly registered!\n")
+	} else {
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Println("response Body:", string(body))
+		fmt.Fprintf(os.Stdout, "Registry failed. HTTP status: %d, HTTP body: %v.\n", response.StatusCode, body)
+		response.Body.Close()
+	}
+}
 
 func killTime(quit chan bool) {
 	ticker := time.NewTicker(5 * time.Second)
@@ -56,6 +112,13 @@ func main() {
 	go killTime(quit)
 
 	server := initializeAPI()
+
+	l, err := server.HTTPListener()
+	if err != nil {
+		panic(err)
+	}
+
+	register(1, l.Addr(), string(restapi.SwaggerJSON))
 
 	if err := server.Serve(); err != nil {
 		panic(err)
